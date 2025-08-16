41c827d76f98e1ad5ed22eff3969958ed77de9dd
Hoang Van Sang <sangskyred@gmail.com>
2025-08-16 22:35:10 +0700

    ok

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Enables static HTML export
+  images: {
+    unoptimized: true, // Disable image optimization if not using a custom loader
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/autocicd/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/autocicd' : '',
 };
 
 export default nextConfig;
